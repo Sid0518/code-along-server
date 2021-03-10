d33aee952e34547564d166af7770ef802da080d7
Siddharth Shekhar <30648642+Sid0518@users.noreply.github.com>
2021-03-10 16:46:25 +0530

    Added functions for executing Python and JavaScript code
    Now the client will be able to send an "executeCode" event via the socket connection, which will contain the code and programming  language.
    
    The server can then store the code into a file and then execute it, and send the output of the code, along with the errors, if any, back to the clients in the room, as a "codeOutput" event.
    
    Currently, only Python 3 or JavaScript code may be executed on the server.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const socket = require("socket.io");
 
+const path = require("path");
+const fs = require("fs");
+const { exec } = require("child_process");
+const { v4: uuid } = require("uuid");
+
 const app = express();
 app.get("/", (req, res) => res.send("Hello World!"));
 
@@ -34,12 +39,69 @@ io.on("connection", (socket) => {
   socket.on("draw", (data) => {
     const rooms = socket.rooms.values();
     for (const room of rooms)
-      socket.to(room).emit("draw", data);
+      if(room !== socket.id)
+        socket.to(room).emit("draw", data);
   });
 
   socket.on("changedCode", (data) => {
     const rooms = socket.rooms.values();
     for (const room of rooms)
-      socket.to(room).emit("changedCode", data);
+      if(room !== socket.id)
+        socket.to(room).emit("changedCode", data);
   });
+
+  socket.on("executeCode", (data) => {
+    const code = data.code;
+    const lang = data.lang;
+
+    const rooms = socket.rooms.values();
+    for(const room of rooms) {
+      if(room !== socket.id)
+        execute_code(code, lang, socket, room);
+    }
+  })
 });
+
+const EXTENSIONS = {
+  "python": "py",
+  "javascript": "js",
+};
+
+const execute_code = async (code, lang, socket, room) => {
+  const codeFile = room;
+  const ext = EXTENSIONS[lang];
+  
+  // const codeLocation = path.join("received_codes", `${codeFile}.${ext}`);
+  const codeLocation = `${codeFile}.${ext}`;
+  fs.writeFile(codeLocation, code, (error) => {
+    if(error)
+      throw error;
+  });
+
+  let command = "";
+  switch(lang) {
+    case("python"):
+      command = `python \"${codeLocation}\"`;
+      break;
+    case("javascript"):
+      command = `node \"${codeLocation}\"`;
+      break;
+  }
+
+  exec(command, (error, stdout, stderr) => {
+    socket.to(room).emit("codeOutput", {
+      error: error,
+      stderr: stderr,
+      stdout: stdout, 
+    });
+
+    if(error)
+      console.log(`error: ${error.message}`);
+        
+    else if(stderr)
+      console.log(`stderr: ${stderr}`);
+        
+    else
+      console.log(`stdout: ${stdout}`);
+  });
+}
