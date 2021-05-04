b98c47db658e359e9df9d515361173488ac3d34f
Ritik Goyal <Phoenix.rg0147@gmail.com>
2021-05-04 22:50:09 +0530

    whole state of room is send to client in zip format

--- download.js
+++ download.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+var AdmZip = require("adm-zip");
+const fs = require("fs");
 
 router.post("/", (req, res) => {
   //     if (!req.body.file || !req.body.roomId) {
@@ -12,6 +14,24 @@ router.post("/", (req, res) => {
   const filePath = __dirname + `/files/${req.body.roomId}/` + file;
 
   res.download(filePath);
+});
+
+router.post("/all", (req, res) => {
+  const folderLocation = __dirname + `/files/${req.body.roomId}`;
+  let zip = new AdmZip();
+  var uploadDir = fs.readdirSync(folderLocation);
+
+  for (var i = 0; i < uploadDir.length; i++) {
+    zip.addLocalFile(`${folderLocation}/` + uploadDir[i]);
+  }
+
+  const downloadName = `State of ${req.body.roomId}.zip`;
+
+  const data = zip.toBuffer();
+  res.set("Content-Type", "application/octet-stream");
+  res.set("Content-Disposition", `attachment; filename=${downloadName}`);
+  res.set("Content-Length", data.length);
+  res.send(data);
 });
 
 module.exports = router;
